a30f17ee544363e7de6e0fde00a9f402c7734b31
alexborgessjc <alexborgessjc@hotmail.com>
2022-05-17 18:11:36 -0300

    alterar categoria

--- alterar_categoria.php
+++ alterar_categoria.php
@@ -1,18 +1,30 @@
 <?php
+    //Atribuindo os valores passados por POST
     $codigo_categoria = $_POST['select_alterar'];
-    $novo_texto = $_POST['c_novo_texto'];
-
-    echo "Página de atualização";
-
-    echo"<br>O código é: ".$codigo_categoria;
-    echo"<br>O novo nome é: ".$novo_texto;
+    $novo_texto = $_POST['c_novo_texto'];   
+    
     //Verificar se existe a categoria cadastrada!
+    //Importando a conexao
+    require("connect.php");
+
     //Criando a SQL de pesquisa
     $sql_pesquisa = 
-    "SELECT * FROM `categoria` WHERE `descricao_categoria` = $codigo_categoria";
+    "SELECT * FROM `categoria` WHERE `descricao_categoria` = '$novo_texto'";
+
+    //executando a pesquisa
+    $resultado_pesquisa = mysqli_query($conexao,$sql_pesquisa); 
     
     //Transformando a pesquisa em numero!
-    $numero_resultado = mysqli_num_rows()
+    $numero_resultado = mysqli_num_rows($resultado_pesquisa);   
+
     //Se não existir... cadastra!
     //Se existir... voltamos para o formulario!
+    if($numero_resultado != 0)
+    {
+        echo "Existe categoria com este nome já cadastrado!";
+    }
+    else
+    {
+        echo "Pode cadastrar!";
+    }
 ?>
