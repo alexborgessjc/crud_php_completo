62922089d58261ff76aee33311a6ac5b394aaafc
alexborgessjc <alexborgessjc@hotmail.com>
2022-06-20 07:42:46 -0300

    Movendo a imagem com o "move_uploaded_file"

--- cadastrar_jogo.php
+++ cadastrar_jogo.php
@@ -8,11 +8,47 @@
      
     //Mostrando informações da imagem
     if(isset($_FILES['arquivo']['name']) && $_FILES["arquivo"]["error"] == 0)
-	{				
+	{			
+        //Mostrando na tela as ifnormações do arquivo enviado	
 		echo "Você enviou o arquivo: <strong>" . $_FILES['arquivo']['name'] . "</strong><br />";
 		echo "Este arquivo é do tipo: <strong>" . $_FILES['arquivo']['type'] . "</strong><br />";
 		echo "Temporáriamente foi salvo em: <strong>" . $_FILES['arquivo']['tmp_name'] . "</strong><br />";
 		echo "Seu tamanho é: <strong>" . $_FILES['arquivo']['size'] . "</strong> Bytes<br /><br />";
+        
+        //Armazenando o nome do arquivo e o local da imagem(o local temporário)
+        $nome = $_FILES['arquivo']['name'];
+        $arquivo_tmp = $_FILES['arquivo']['tmp_name'];
+								
+		//Obtendo a extensão do arquivo e aramzenando em uma variável
+		$extensao = strrchr($nome, '.');
+				
+		//Convertendo o texto em minúsculo
+		$extensao = strtolower($extensao);
+
+        if(strstr('.jpg;.jpeg;.gif;.png', $extensao))
+		{ 
+            // Cria um nome único para esta imagem
+			// Evita que duplique as imagens no servidor.
+			$novoNome = md5(microtime()) . $extensao;
+					
+			// Concatena a pasta com o nome
+			$destino = 'images/capas/' . $novoNome; 
+
+            // tenta mover o arquivo para o destino
+			if( @move_uploaded_file( $arquivo_tmp, $destino  ))
+			{
+				echo "Arquivo salvo com sucesso em : <strong>" . $destino . "</strong><br />";
+				echo "<img src=\"" . $destino . "\" />";									
+			}
+			else
+			{	
+				echo "Erro ao salvar o arquivo.<br/>";
+			}
+        }
+        else
+        {
+            echo "Você poderá enviar apenas arquivos \"*.jpg;*.jpeg;*.gif;*.png\"<br />";
+        }
     }
     else
 	{
